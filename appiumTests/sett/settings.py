aa18d57b90c8e0cccbd8142620f40776f7a0875f
Goldyn <robert@gociety.com>
2015-08-31 13:13:57 +0200

    asdfs

--- appiumTests/sett/settings.py
+++ appiumTests/sett/settings.py
@@ -8,5 +8,5 @@ emu= 'Android emulator'
 #appiumHub
 appiumHub = 'http://localhost:4723/wd/hub'
 #appium Point the driver address to use the Sauce Labs cloud
-appiumHub2 = 'http://r0850n:9b73aa77-417e-4872-b062-9a270156fa9a@ondemand.saucelabs.com:80/wd/hub'
+appiumHub2 = 'http://r0850n:3b76db4e-16d5-4a76-a791-45f3b41e53fc@ondemand.saucelabs.com:80/wd/hub'
 
