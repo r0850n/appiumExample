cc53218324d8b89328c1f543398c5f9a86aa52ae
Goldyn <robert@gociety.com>
2015-09-01 15:42:06 +0200

    aSD

--- appiumTests/sett/loadDriver.py
+++ appiumTests/sett/loadDriver.py
@@ -2,15 +2,20 @@ from appium import webdriver
 from sett import settings
 import os
 
-PATH = lambda p: os.path.abspath(
-    os.path.join(os.path.dirname(__file__), p)
-)
+from sauceclient import SauceClient
+
+
+
+SAUCE_USERNAME = os.environ.get('SAUCE_USERNAME')
+SAUCE_ACCESS_KEY = os.environ.get('SAUCE_ACCESS_KEY')
+sauce = SauceClient(SAUCE_USERNAME, SAUCE_ACCESS_KEY)
 
 class LoadDriver(object):
 
     @staticmethod
     def loadAppiumDriver():
-
+        sauce_url = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
+        
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
         desired_caps['platformVersion'] = '4.3'
@@ -21,4 +26,4 @@ class LoadDriver(object):
         desired_caps['appActivity'] = '.activity.Main'
         # Adding appWait Activity since the activity name changes as the focus shifts to the ATP WTA app's first page
         desired_caps['appWaitActivity'] = '.activity.root.TournamentList'
-        return webdriver.Remote(settings.appiumHub2, desired_caps)
+        return webdriver.Remote(sauce_url, desired_caps)
